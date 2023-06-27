148185c2f5014728a6ca5bf57bc6a8cdc2463f47
Nicolas Primo <nicolasprimo@gmail.com>
2023-06-27 08:01:13 -0300

    input changes and contactusposter changes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 
-const Input = ({ type, content }) => {
+const Input = ({ type, label, steps }) => {
   return (
-    <div className="flex flex-col gap-[1px]">
+    <div className={`flex flex-col gap-[1px] `}>
       <label
         htmlFor={type}
         className="text-[13px] pl-[2px] font-medium tracking-[0.15rem] "
       >
-        {content}
+        {label}
       </label>
-      <input
-        type={type}
-        className="rounded-[7px] w-[210px] h-[28px] bg-white bg-opacity-[0.20]"
-      />
+      {steps == 3 ? (
+        <textarea
+          name="content"
+          id=""
+          cols="70"
+          rows="6"
+          className="bg-white bg-opacity-[0.20] focus:bg-opacity-25 outline-0 px-2 text-[14px] rounded-[7px] py-2 resize-none"
+        ></textarea>
+      ) : (
+        <input
+          type={type}
+          className={`rounded-[7px] w-[210px] h-[28px] py-4 bg-white bg-opacity-[0.20] focus:bg-opacity-25 outline-0 px-2 text-[14px]`}
+        />
+      )}
     </div>
   );
 };
--- src/components/posters/ContactUsPoster.jsx
+++ src/components/posters/ContactUsPoster.jsx
@@ -2,42 +2,110 @@ import React, { useState } from "react";
 import Input from "../Input";
 import "./ContactUsPoster.css";
 
-const ContactUsPoster = () => {
+const ContactUsPosterSteps = ({ steps }) => {
   const [inputContent, setInputContent] = useState([
     {
       type: "text",
-      content: "Name",
+      label: "Name",
+      content: "",
     },
     {
       type: "text",
-      content: "Company",
+      label: "Company",
+      content: "",
     },
     {
       type: "email",
-      content: "Email",
+      label: "Email",
+      content: "",
     },
   ]);
-
+  const [servicesButtons, setServicesButtons] = useState([
+    "Services",
+    "Products",
+    "Projects",
+    "Others",
+  ]);
   return (
-    <div className="flex items-center justify-center w-full ">
-      <div className="h-[400px] w-[900px] bg-white contact_poster_background rounded-[11.3391px] ">
-        <div className="flex justify-end w-full pt-8 pr-8 h-7">
-          <span className="text-xs font-medium tracking-[0.15rem] text-white text-opacity-[0.7]">
-            01/03
-          </span>
-        </div>
-        <div className="h-full flex flex-col gap-[40px] items-center justify-center text-opacity-[0.70] text-white">
+    <>
+      {steps == 1 ? (
+        <>
           <span className="font-medium text-[18px] tracking-[0.15rem]">
             Hi, give us some information about your company
           </span>
           <div className="flex gap-[70px]">
             {inputContent.map((input) => (
-              <Input type={input.type} content={input.content} />
+              <Input type={input.type} label={input.label} />
             ))}
           </div>
-          <button className="bg-white bg-opacity-[0.20] px-[16px] py-[4px] rounded-[6px] text-[13px] font-semibold">
-            Next
-          </button>
+        </>
+      ) : steps == 2 ? (
+        <>
+          <span className="font-medium text-[18px] tracking-[0.15rem]">
+            Now, tell us what kind of service you are looking for
+          </span>
+          <div className="flex gap-[70px]">
+            {servicesButtons.map((service) => (
+              <button className="w-[85px] h-[85px] text-center bg-white rounded-full bg-opacity-20 text-[13px] font-semibold focus:bg-opacity-25">
+                {service}
+              </button>
+            ))}
+          </div>
+        </>
+      ) : steps == 3 ? (
+        <>
+          <span className="font-medium text-[18px] tracking-[0.15rem]">
+            And finally, tell us what specifically you're looking for
+          </span>
+          <div className="flex gap-[70px]">
+            <Input label={"Content"} type={"text"} steps={steps} />
+          </div>
+        </>
+      ) : (
+        "error"
+      )}
+    </>
+  );
+};
+
+const ContactUsPoster = () => {
+  const [steps, setSteps] = useState(1);
+  return (
+    <div className="flex items-center justify-center w-full ">
+      <div className="h-[400px] w-[900px] bg-white contact_poster_background rounded-[11.3391px] ">
+        <div className="flex justify-end w-full pt-8 pr-8 h-7">
+          <span className="text-xs font-medium tracking-[0.15rem] text-white text-opacity-[0.7]">
+            0{steps}/03
+          </span>
+        </div>
+        <div className="h-full w-full flex flex-col gap-[35px] items-center justify-center text-opacity-[0.70] text-white">
+          <ContactUsPosterSteps steps={steps} />
+
+          <div className="flex items-center justify-center gap-8">
+            <button
+              onClick={() => {
+                if (steps > 1) {
+                  setSteps(steps - 1);
+                }
+              }}
+              className={`bg-white bg-opacity-[0.20] px-[16px] py-[4px] rounded-[6px] text-[13px] font-semibold ${
+                steps > 1 ? "block" : "hidden"
+              }`}
+            >
+              Previous
+            </button>
+            <button
+              onClick={() => {
+                if (steps < 3) {
+                  setSteps(steps + 1);
+                } else if (steps == 3) {
+                }
+              }}
+              className="bg-white bg-opacity-[0.20] px-[16px] py-[4px] rounded-[6px] text-[13px] font-semibold"
+            >
+              {steps < 3 ? "Next" : "Submit"}
+            </button>
+          </div>
         </div>
       </div>
     </div>
