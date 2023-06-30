5da90ef80168104405e198622e5c480ce7bf0829
Nicolas Primo <nicolasprimo@gmail.com>
2023-06-30 13:51:13 -0300

    fixing some issues

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -7,7 +7,7 @@ const Section = ({ section_title, section_subtitle, poster, about_poster }) => {
   return (
     <div className="flex flex-col gap-16 mx-[22px] md:mx-[48px] border-y-[2px] h-[101vh] overflow-y-hidden overflow-x-hidden border-white border-opacity-40 text-white justify-center">
       <div className="flex flex-col">
-        <span className="text-[25px] md:text-[45px] font-black ml-[-2px]">
+        <span className="text-[25px] md:text-[45px] font-black">
           {section_title}
         </span>
         <span className="text-[11px] md:text-[13px] font-normal tracking-[0.17rem] w-48">
--- src/components/posters/AboutPoster.jsx
+++ src/components/posters/AboutPoster.jsx
@@ -3,14 +3,14 @@ import "./AboutPoster.css";
 
 const AboutPoster = () => {
   return (
-    <div className="flex flex-col items-center w-full md:justify-between md:pr-[70px] md:flex-row">
-      <div className="w-[230px] h-[200px] md:w-[900px] md:h-[300px] about_poster_background rounded-[40px] border-[1px] border-white border-opacity-60 ">
-        <span></span>
+    <div className="flex flex-col items-center  md:justify-between md:pr-[70px] md:flex-row ">
+      <div className="hidden flex-col justify-start items-start overflow-clip  w-[260px]  h-[400px] md:w-[900px] md:h-[300px] about_poster_background rounded-[40px] border-[1px] border-white border-opacity-60 ">
+        <div className="flex items-center justify-center w-full px-4 overflow-y-visible text-justify"></div>
       </div>
       <img
         src="./argos_logo_uni.svg"
         alt=""
-        className="scale-[0.20] md:scale-[0.85]"
+        className=" md:flex scale-[0.85] "
       />
     </div>
   );
